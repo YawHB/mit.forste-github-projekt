f7ad1d66cc8193b5fa57726ecce8e90b58d79102
YawHB <yawhboateng@gmail.com>
2023-01-31 13:14:55 +0100

    console.log øvelse
    Jeg har lært at logge til konsolen.

---
+++ app.js
@@ -0,0 +1,6 @@
+document.body.innerHTML += "Hello JavaScript!";
+document.body.innerHTML += "Hello JavaScript!";
+//alert("Hello Dat.js");
+console.log("Hello Dat.js");
+console.log("Fræk mic");
+console.log(" i skuret");
